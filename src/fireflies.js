98152074a074b6e3ef59e7bcc46f62b341aa24a6
Arisien <arisien@vivaldi.net>
2021-05-28 12:10:58 +0200

    Fix mouse interactions

--- src/fireflies.js
+++ src/fireflies.js
@@ -5,6 +5,7 @@ const mouse = {
   x: undefined,
   y: undefined
 }
+let maxRadius
 const fireflies = []
 const particles = []
 const animationLoop = _ => {
@@ -24,7 +25,7 @@ const mouseEH = _ => {
 }
 
 export default class Fireflies {
-  static initialize(quantity = Math.floor((window.innerHeight + window.innerWidth) / 100), radius = [5, 25 + Math.floor((window.innerHeight + window.innerWidth) / 100)], color = [{ fill: '#ffffea', glow: '#ff881b' }], collision = false, pulse = true, flicker = true, connect = false) {
+  static initialize(quantity = Math.floor((window.innerHeight + window.innerWidth) / 100), radius = [5, 25 + Math.floor((window.innerHeight + window.innerWidth) / 100)], color = [{ fill: '#ffffea', glow: '#ff881b' }], collision = false, pulse = true, flicker = true, connect = false, charge = 1) {
     this.terminate() // Terminates all previously initialized instances
     canvas = document.createElement('canvas')
     document.body.appendChild(canvas)
@@ -36,13 +37,15 @@ export default class Fireflies {
       let r
       if (Object.prototype.toString.call(radius) === '[object Array]') {
         r = rng(radius[0], radius[1])
+        maxRadius = radius[1]
       } else {
         r = radius
+        maxRadius = radius
       }
       const x = rng(r, canvas.width - r)
       const y = rng(r, canvas.height - r)
       const randomColor = rcg(color)
-      fireflies[i] = new Firefly(x, y, r, randomColor, collision, pulse, flicker, connect)
+      fireflies[i] = new Firefly(x, y, r, randomColor, collision, pulse, flicker, connect, charge)
     }
     addEventListener('resize', resizeEH)
     addEventListener('mousemove', mouseEH)
@@ -61,10 +64,11 @@ export default class Fireflies {
   }
 }
 export class Firefly {
-  constructor(x, y, radius, color, collision, pulse, flicker, connect) {
+  constructor(x, y, radius, color, collision, pulse, flicker, connect, charge) {
     this.x = x
     this.y = y
     this.radius = radius
+    this.charge = charge
     this.color = {
       fill: color.fill,
       glow: color.glow
@@ -96,17 +100,32 @@ export class Firefly {
   }
   fly() {
     this.collide()
+    this.field();
     this.stayWithinView() // Screenbound
-    if (distance(this.x, this.y, mouse.x, mouse.y) < this.radius) {
-      this.x += 0.75 * Math.cos(this.velocity.x) * -1
-      this.y += 0.75 * Math.sin(this.velocity.y) * -1
-    } else {
-      this.x += 0.75 * Math.cos(this.velocity.x) // The number is the speed modifier
-      this.y += 0.75 * Math.sin(this.velocity.y) // The number is the speed modifier
-    }
+    this.x += 0.75 * Math.cos(this.velocity.x) // The number is the speed modifier
+    this.y += 0.75 * Math.sin(this.velocity.y) // The number is the speed modifier
     this.calcGlow()
     // this.leaveTrail()
     this.draw()
+  }
+  field() {
+    if (this.charge == 0) return;
+    const k = 3 //Kind of like coloumbs constant
+
+    let deltaX = this.x - mouse.x // Horizontal distance between firefly and mouse
+    let deltaY = this.y - mouse.y // Vertical distance between firefly and mouse
+
+    let distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2)) // Distance between firefly and mouse
+    let angle = Math.atan(Math.abs(deltaY) / Math.abs(deltaX)) // Angle of firefly with respect to mouse
+
+    let velocity = k * this.charge / Math.pow(distance/maxRadius, 2) // Velocity is modelled after electric field (inaccurate more conversions needed for true velocity)
+    if (velocity > k * this.charge) velocity = k * this.charge // Sets limit on field strength, this is in case mouse is within the radius
+
+    let vx = velocity * Math.cos(angle) * (deltaX/Math.abs(deltaX)) // Horizontal component of v is cos of angle times net velocity as well as the direction
+    let vy = velocity * Math.sin(angle) * (deltaY/Math.abs(deltaY)) // Vertical component of v is sin of angle times net velocity as well as the direction
+
+    if (Number.isFinite(vx)) this.x += vx //Arctan function causes some NaN numbers for fireflies, ignore these
+    if (Number.isFinite(vy)) this.y += vy
   }
   stayWithinView() {
     // particle.position.x = Math.max( Math.min( particle.position.x, SCREEN_WIDTH ), 0 );
